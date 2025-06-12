b3984ac6d016311dae02037b8c566767e96ac6d7
user <user@Soumen.local>
2025-06-12 15:57:04 +0600

    SubCategory  model has be created

---
+++ Server/models/subCategory.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const subCategorySchema = new mongoose.Schema({
+    name:{
+        type:String,
+        default:""
+    },
+    Image:{
+        type: String,
+        default:""
+    },
+    categoryId:{
+        type: mongoose.Schema.ObjectId,
+        ref: "category"
+    }
+},{
+    timestamps:true
+})
